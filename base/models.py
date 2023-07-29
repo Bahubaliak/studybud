a67792f038fe2629f92dacf164a4ef6940b9cee0
bahubali_ak <bahubalikurali@gmail.com>
2023-07-29 17:47:15 +0530

    handle avatar

--- base/models.py
+++ base/models.py
@@ -1,15 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-def default_avatar_path(instance, filename):
-    return f'avatars/{filename}'
-
 class User(AbstractUser):
     name = models.CharField(max_length=200, null=True)
     email = models.EmailField(unique=True, null=True)
     bio = models.TextField(null=True)
 
-     avatar = models.ImageField(null=True, upload_to=default_avatar_path, default="avatars/avatar.svg")
+    avatar = models.ImageField(null=True, default="avatars/avatar.svg")
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
--- studybud/settings.py
+++ studybud/settings.py
@@ -149,7 +149,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 
 
-MEDIA_ROOT = BASE_DIR / 'static/images'
+MEDIA_ROOT = BASE_DIR / 'static/images/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
